fd04293dc2f1ad75a379830705487fb90436b185
Xempii <gartoonqqq@gmail.com>
2023-04-04 22:56:21 +0700

    Fix

--- lib/Screen/timeRemining.dart
+++ lib/Screen/timeRemining.dart
@@ -1,8 +1,14 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/Screen/checkout.dart';
 import 'package:myapp/Widget/styles.dart';
 import 'package:slide_countdown/slide_countdown.dart';
+
+import '../flutter_flow/flutter_flow_util.dart';
 
 class timeRemining extends StatefulWidget {
   const timeRemining({super.key});
@@ -12,7 +18,44 @@ class timeRemining extends StatefulWidget {
 }
 
 class _timeReminingState extends State<timeRemining> {
-  Duration _duration = Duration(hours: 2);
+  String get userUID {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return 'No user';
+    } else {
+      return user.uid;
+    }
+  }
+
+  Future<Map<String, dynamic>?> getUserBookinfo() async {
+    log('user ID :' + userUID);
+    log('GET USER BOOKING INFO');
+    final userRef = FirebaseFirestore.instance
+        .collection('/app/member/ID/$userUID/Booking');
+    final userBookings = await userRef.get();
+    if (userBookings.docs.isEmpty) {
+      // Show Dialog
+      log('YOU ARE NOT BOOKING');
+    } else {
+      log('YOU BOOKING');
+      for (var booking in userBookings.docs) {
+        log('YOUR BOOKING ID : ' + booking.id);
+        final bookingData = booking.data();
+        if (bookingData.isNotEmpty) {
+          log('Booking is not emtpy');
+        }
+        return bookingData;
+      }
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserBookinfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,170 +79,130 @@ class _timeReminingState extends State<timeRemining> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      child: Image.asset(
-                        'assets/messageImage_1677661037176.jpg',
-                        width: 300,
-                        height: 250,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      child: Text(
-                        "Parking time",
-                        style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(0, 0, 0, 1),
+        child: FutureBuilder(
+          future: getUserBookinfo(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              Map<String, dynamic> data = snapshot.data!;
+              Duration duration =
+                  Duration(hours: (data['Duration'] / 60.0).toInt());
+              var timeRemaining = duration;
+              var dateString = DateFormat('yyyy-MM-dd').format(DateTime.now());
+              var startTimeString = data['Start'];
+              var fullDateTimeString = '$dateString $startTimeString';
+              var startTime =
+                  DateFormat('yyyy-MM-dd h:mm a').parse(fullDateTimeString);
+              var currentTime = DateTime.now();
+              log('startTime : ' + startTime.toString());
+              log('currentTime : ' + currentTime.toString());
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/messageImage_1677661037176.jpg',
+                              width: 300,
+                              height: 250,
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Container(
+                            child: Text(
+                              data['Type'] + ' time',
+                              style: GoogleFonts.montserrat(
+                                textStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          currentTime.isBefore(startTime)
+                              ? Text('Not time yet ,Your booking time is ' +
+                                  data['Start'])
+                              : Container(
+                                  child: Center(
+                                    child: SlideCountdown(
+                                      duration: timeRemaining,
+                                      onDone: () {
+                                        timeRemaining -= Duration(seconds: 1);
+                                      },
+                                      slideDirection: SlideDirection.down,
+                                      separator: ":",
+                                      separatorType: SeparatorType.symbol,
+                                      decoration:
+                                          BoxDecoration(color: Colors.white),
+                                      textStyle: GoogleFonts.montserrat(
+                                        textStyle: TextStyle(
+                                          fontSize: 50,
+                                          fontWeight: FontWeight.w700,
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                        ],
                       ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      child: Center(
-                        child: SlideCountdown(
-                          duration: _duration,
-                          slideDirection: SlideDirection.down,
-                          separator: ":",
-                          separatorType: SeparatorType.symbol,
-                          decoration: BoxDecoration(color: Colors.white),
-                          textStyle: GoogleFonts.montserrat(
-                            textStyle: TextStyle(
-                              fontSize: 50,
-                              fontWeight: FontWeight.w700,
-                              color: Color.fromRGBO(0, 0, 0, 1),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: ((context) => checkout()),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 170,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Finished",
+                              style: GoogleFonts.montserrat(
+                                textStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: ((context) => checkout()),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: 170,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Finished",
-                        style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            color: Color.fromRGBO(0, 0, 0, 1),
-                          ),
-                        ),
-                      ),
-                    ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ],
+                ],
+              );
+            } else {
+              return Center(child: Text("You haven't made booking"));
+            }
+          },
         ),
       ),
     );
   }
 }
-
-// Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: [
-//             Container(
-//               child: Image.asset(
-//                 'assets/messageImage_1677661037176.jpg',
-//                 width: 300,
-//                 height: 250,
-//               ),
-//             ),
-//             Container(
-//               child: Text(
-//                 "Parking time",
-//                 style: GoogleFonts.montserrat(
-//                   textStyle: TextStyle(
-//                     fontSize: 20,
-//                     fontWeight: FontWeight.w600,
-//                     color: Color.fromRGBO(0, 0, 0, 1),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             Container(
-//               child: Center(
-//                 child: SlideCountdown(
-//                   duration: _duration,
-//                   slideDirection: SlideDirection.down,
-//                   separator: ":",
-//                   separatorType: SeparatorType.symbol,
-//                   decoration: BoxDecoration(color: Colors.white),
-//                   textStyle: GoogleFonts.montserrat(
-//                     textStyle: TextStyle(
-//                       fontSize: 50,
-//                       fontWeight: FontWeight.w700,
-//                       color: Color.fromRGBO(0, 0, 0, 1),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             GestureDetector(
-//               onTap: () {
-//                 // Navigator.of(context).push(
-//                 //   MaterialPageRoute(
-//                 //     builder: ((context) => checkOut()),
-//                 //   ),
-//                 // );
-//               },
-//               child: Container(
-//                 width: 150,
-//                 height: 50,
-//                 decoration: BoxDecoration(
-//                   color: Colors.blue,
-//                   borderRadius: BorderRadius.circular(18),
-//                 ),
-//                 child: Center(
-//                   child: Text(
-//                     "Finished",
-//                     style: GoogleFonts.montserrat(
-//                       textStyle: TextStyle(
-//                         fontSize: 20,
-//                         fontWeight: FontWeight.w500,
-//                         color: Color.fromRGBO(0, 0, 0, 1),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
