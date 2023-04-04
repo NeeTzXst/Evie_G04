2b7ba145df928229b0f4d0145fe3b8adcd00dfb6
Cheesey <aueangkoon.k@ku.th>
2023-04-04 15:57:44 +0700

    Add Database

--- lib/Screen/Drawer/history.dart
+++ lib/Screen/Drawer/history.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, sized_box_for_whitespace
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/Widget/styles.dart';
+import '/Widget/styles.dart';
 
 class history extends StatefulWidget {
   const history({super.key});
@@ -9,6 +12,8 @@ class history extends StatefulWidget {
 }
 
 class _historyState extends State<history> {
+  String get userUID => FirebaseAuth.instance.currentUser!.uid;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +35,162 @@ class _historyState extends State<history> {
           "History",
           style: headerText,
         ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('app')
+            .doc('member')
+            .collection('ID')
+            .doc(userUID)
+            .collection("History")
+            .orderBy("Time", descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        side: BorderSide(color: Colors.black, width: 0.1),
+                      ),
+                      elevation: 7,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                          hintStyle: BlueDisplay,
+                          prefixIcon: Icon(Icons.search,
+                              color: Color.fromRGBO(26, 116, 226, 1)),
+                        ),
+                      ),
+                    ))
+              ],
+            );
+          }
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        side: BorderSide(color: Colors.black, width: 0.1),
+                      ),
+                      elevation: 7,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                          hintStyle: BlueDisplay,
+                          prefixIcon: Icon(Icons.search,
+                              color: Color.fromRGBO(26, 116, 226, 1)),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 9),
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25.0),
+                                    side: BorderSide(
+                                        color: Colors.black, width: 0.01),
+                                  ),
+                                  color: Color.fromRGBO(196, 236, 255, 0.839),
+                                  elevation: 7,
+                                  child: Container(
+                                    height: 100,
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          width: 350,
+                                          child: Row(
+                                            children: [
+                                              SizedBox(
+                                                height: 10,
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 6),
+                                                child: ClipRRect(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          100),
+                                                  child: Image.network(
+                                                    'https://cdiz-84c1.kxcdn.com/rent-space/uploads/post/130665-2.jpg',
+                                                    width: 110,
+                                                    height: 75,
+                                                  ),
+                                                ),
+                                              ),
+                                              Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Padding(
+                                                    padding: EdgeInsets.only(
+                                                        left: 6, top: 8),
+                                                    child: Text(
+                                                      "${snapshot.data!.docs[index].get('Station')}",
+                                                      style: StationFull,
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding: EdgeInsets.only(
+                                                      left: 6,
+                                                    ),
+                                                    child: Text(
+                                                      "${snapshot.data!.docs[index].get('Price')} Baht.",
+                                                      style: itemDrawerText,
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding: EdgeInsets.only(
+                                                      left: 6,
+                                                    ),
+                                                    child: Text(
+                                                      "${snapshot.data!.docs[index].get('License Number')}",
+                                                      style: VehicleName,
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }))
+              ],
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
