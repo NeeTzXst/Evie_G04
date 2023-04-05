18a74d744da83e64431760ad1b40d781d04e2953
Cheesey <aueangkoon.k@ku.th>
2023-04-05 19:01:05 +0700

    edit UI

--- lib/Screen/Drawer/myPayment.dart
+++ lib/Screen/Drawer/myPayment.dart
@@ -1,95 +1,148 @@
+// ignore_for_file: unused_local_variable
 import 'package:flutter/material.dart';
+import 'package:myapp/Screen/homePage.dart';
+import 'package:myapp/Widget/styles.dart';
 
-import '../../flutter_flow/flutter_flow_theme.dart';
-import '../../flutter_flow/flutter_flow_widgets.dart';
 import '../AddCreditCard.dart';
-import '../homePage.dart';
 
-class myPayment extends StatefulWidget {
-  const myPayment({super.key});
-
-  @override
-  State<myPayment> createState() => _MyWidgetState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyWidgetState extends State<myPayment> {
-  final _unfocusNode = FocusNode();
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: myPayment(),
+    );
+  }
+}
+
+class myPayment extends StatefulWidget {
+  const myPayment({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  myPaymentState createState() => myPaymentState();
+}
+
+class myPaymentState extends State<myPayment> {
+  bool isObscure = true;
+  @override
+  Widget build(BuildContext context) {
+    FocusNode? _unfocusNode;
     return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        leading: Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
-          child: InkWell(
-            onTap: () async {
+        //  extendBodyBehindAppBar: true,
+        floatingActionButton: SizedBox(
+          width: 350,
+          height: 60,
+          child: FloatingActionButton.extended(
+            backgroundColor: secondColor,
+            label: Text(
+              "Add New Credit Card",
+              style: headerText,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16.0)),
+            ),
+            onPressed: () {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => homePage()));
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => AddCreditCardWidget()));
+            },
+          ),
+        ),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: GestureDetector(
+            onTap: () {
+              print("pressed");
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => homePage(),
+                ),
+              );
             },
             child: Icon(
               Icons.arrow_back,
-              color: Color(0xFF1A74E2),
-              size: 33,
+              size: 40,
+              color: Color.fromRGBO(26, 116, 226, 1),
             ),
           ),
-        ),
-        title: Align(
-          alignment: AlignmentDirectional(-1, 0),
-          child: Text(
-            'My Payment Method',
-            style: TextStyle(
-              fontFamily: 'Montserrat',
-              color: Color(0xFF1A74E2),
-            ),
+          title: Text(
+            "Payment Methods",
+            style: headerText,
           ),
         ),
-        actions: [],
-        centerTitle: false,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
-          child: Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(32, 0, 0, 32),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FFButtonWidget(
-                  onPressed: () {
-                    print("Add Card pressed ...");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AddCreditCardWidget()));
-                  },
-                  text: 'Add Credit Card\n',
-                  options: FFButtonOptions(
-                    width: 330,
-                    height: 55,
-                    color: Color.fromARGB(216, 107, 208, 255),
-                    textStyle: FlutterFlowTheme.of(context).subtitle2.override(
-                          fontFamily: 'Montserrat',
-                          color: Color(0xFF1A74E2),
-                          fontSize: 20,
+        body: ListView(children: <Widget>[
+          Container(
+              width: 460,
+              // height: 510,
+              child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          // padding: const EdgeInsets.symmetric(horizontal: 10),
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(10, 10, 10, 0),
+                          child: GestureDetector(
+                            onTap: () {
+                              // Perform the desired action when the card is tapped.
+                              print("card pressed..");
+                            },
+                            child: Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                              child: SizedBox(
+                                height: 120,
+                                width: 360,
+                                child: Card(
+                                  elevation: 0,
+                                  color: Color.fromARGB(255, 107, 207, 255),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                        title: Padding(
+                                            padding: EdgeInsets.only(top: 20),
+                                            child: Text(
+                                              '1111 2222 3333 4444',
+                                              style: BlueDisplayBold,
+                                            )),
+                                        subtitle: Padding(
+                                            padding: EdgeInsets.only(top: 20),
+                                            child: Text(
+                                              'Expires 09/28',
+                                              style: BlueDisplayBold,
+                                            )),
+                                        leading: Icon(
+                                          Icons.credit_card,
+                                          size: 80,
+                                          // color: Color(0xFF3FA0EF),
+                                          color: Colors.black,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                      width: 1,
+                      ],
                     ),
-                    borderRadius: 10, decoration: BoxDecoration(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                  ]))
+        ]));
   }
 }
